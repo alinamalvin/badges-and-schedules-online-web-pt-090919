78a6153048fde60f7cba9a5a653ce0a6cc6921cc
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-17 18:03:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,4 +6,6 @@ def batch_badge_creator(array)
   new_array=[]
   array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
   array.each do |name|
-    new_array.push 
+    new_array.push "Hello, my name is #{name}."
+  end
+end 
